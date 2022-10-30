4ed9a1685319655688079d3fdc1074d048c487ac
Rodrigo Cury <rodrigocuryalmeida@gmail.com.br>
2022-10-30 19:01:37 -0300

    Logos

--- src/components/three-assets/logos/index.js
+++ src/components/three-assets/logos/index.js
@@ -1,34 +1,33 @@
 import LogoLoader from './LogoLoader'
 
+// Languages
+
 export const KtLogo = (props) => {
   return <LogoLoader uri='/Kt-logo.glb' {...props} />
-}
-export const AWSLogo = (props) => {
-  return <LogoLoader uri='/aws-logo.glb' {...props} />
-}
-export const EmailLogo = (props) => {
-  return <LogoLoader uri='/eMailLogo.glb' {...props} />
-}
-export const GitHubLogo = (props) => {
-  return <LogoLoader uri='/githubLogo.glb' {...props} />
 }
 export const JavaLogo = (props) => {
   return <LogoLoader uri='/Java.glb' {...props} />
 }
-export const LinkedInLogo = (props) => {
-  return <LogoLoader uri='/lkdLogo.glb' {...props} />
-}
-export const PostgreLogo = (props) => {
-  return <LogoLoader uri='/psql.glb' {...props} />
+export const RxLogo = (props) => {
+  return <LogoLoader uri='/rx.glb' {...props} />
 }
 export const PythonLogo = (props) => {
   return <LogoLoader uri='/pyLogo.glb' {...props} />
 }
-export const ThreeLogo = (props) => {
-  return <LogoLoader uri='/three.glb' {...props} />
+export const PostgreLogo = (props) => {
+  return <LogoLoader uri='/psql.glb' {...props} />
 }
-export const BlenderLogo = (props) => {
-  return <LogoLoader uri='/blender.glb' {...props} />
+export const NodeLogo = (props) => {
+  return <LogoLoader uri='/nodejs.glb' {...props} />
+}
+
+// Backed End Stack
+
+export const SpringLogo = (props) => {
+  return <LogoLoader uri='/spring.glb' {...props} />
+}
+export const SpringRxLogo = (props) => {
+  return <LogoLoader uri='/spring-reactive.glb' {...props} />
 }
 export const DockerLogo = (props) => {
   return <LogoLoader uri='/docker.glb' {...props} />
@@ -39,24 +38,60 @@ export const GitLogo = (props) => {
 export const KafkaLogo = (props) => {
   return <LogoLoader uri='/kafka.glb' {...props} />
 }
+export const MongoLogo = (props) => {
+  return <LogoLoader uri='/mongo.glb' {...props} />
+}
+export const RedisLogo = (props) => {
+  return <LogoLoader uri='/redis.glb' {...props} />
+}
+
+// Front End Stack
+
+export const ReactLogo = (props) => {
+  return <LogoLoader uri='/react.glb' {...props} />
+}
+export const ThreeLogo = (props) => {
+  return <LogoLoader uri='/three.glb' {...props} />
+}
+export const HTMLLogo = (props) => {
+  return <LogoLoader uri='/three.glb' {...props} />
+}
+export const CSSLogo = (props) => {
+  return <LogoLoader uri='/three.glb' {...props} />
+}
+export const JSLogo = (props) => {
+  return <LogoLoader uri='/three.glb' {...props} />
+}
+
+// AWS
+
+export const AWSLogo = (props) => {
+  return <LogoLoader uri='/aws-logo.glb' {...props} />
+}
 export const LocalstackLogo = (props) => {
   return <LogoLoader uri='/linux.glb' {...props} />
 }
 export const AWSServicesLogo = (props) => {
   return <LogoLoader uri='/aws-services.glb' {...props} />
 }
-export const MongoLogo = (props) => {
-  return <LogoLoader uri='/mongo.glb' {...props} />
-}
-export const NodeLogo = (props) => {
-  return <LogoLoader uri='/nodejs.glb' {...props} />
-}
-export const ReactLogo = (props) => {
-  return <LogoLoader uri='/react.glb' {...props} />
-}
-export const RedisLogo = (props) => {
-  return <LogoLoader uri='/redis.glb' {...props} />
-}
+
+// Contact Logos
+
 export const WhatsLogo = (props) => {
   return <LogoLoader uri='/wpp.glb' {...props} />
+}
+export const EmailLogo = (props) => {
+  return <LogoLoader uri='/eMailLogo.glb' {...props} />
+}
+export const GitHubLogo = (props) => {
+  return <LogoLoader uri='/githubLogo.glb' {...props} />
+}
+export const LinkedInLogo = (props) => {
+  return <LogoLoader uri='/lkdLogo.glb' {...props} />
+}
+
+// others
+
+export const BlenderLogo = (props) => {
+  return <LogoLoader uri='/blender.glb' {...props} />
 }
